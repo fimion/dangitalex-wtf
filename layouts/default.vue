302c2c4aa7c16e4a4d39f5ec336cff7937879bf2
Alex Riviere <fimion@users.noreply.github.com>
2021-10-07 12:08:25 +0000

    add good fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,43 @@
+<script>
+export default {
+  head(){
+    return {
+      link:[
+        {
+          rel:"preconnect",
+          href:"https://fonts.googleapis.com",
+        },
+        {
+          rel:"preconnect",
+          href:"https://fonts.gstatic.com",
+          crossorigin:true,
+        },
+        {
+          rel:"stylesheet",
+          href:"https://fonts.googleapis.com/css2?family=Gluten:wght@100..900&family=Grandstander:ital,wght@0,100..900;1,100..900&display=swap",
+        },
+      ]
+    }
+  }
+}
+</script>
+
 <template>
-  <div>
+  <div class="default-layout">
     <header>
-        <h1>Alex's Very Professional Portfolio</h1>
+        <h1><nuxt-link to="/">Alex's Very Professional Portfolio</nuxt-link></h1>
     </header>
     <main>
         <Nuxt />
     </main>
   </div>
 </template>
+
+<style>
+html,body{
+  font-family: 'Grandstander', sans-serif;
+}
+h1,h2,h3,h4,h5,h6{
+    font-family: 'Gluten', cursive;
+  }
+</style>
