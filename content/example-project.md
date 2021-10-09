6383e642089b0bef0002ce08654eca56ea26f949
fimion <fimion@gmail.com>
2021-10-09 10:54:55 -0400

    Add comic mono for correctness

--- content/example-project.md
+++ content/example-project.md
@@ -1,6 +1,12 @@
 ---
 title: An Example Project
-description: It's an Example.
+description: It's an Example. Woooo!
 ---
 
-This is my example project. It's very basic.
+This is my example project. It's very basic. there seems to be some bugs regarding the height of the `nuxt-content` area. awkward.
+
+
+```javascript
+
+const testing = "hello";
+```
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,30 @@
 <script>
 export default {
-  head(){
+  head() {
     return {
-      link:[
+      link: [
         {
-          rel:"preconnect",
-          href:"https://fonts.googleapis.com",
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
         },
         {
-          rel:"preconnect",
-          href:"https://fonts.gstatic.com",
-          crossorigin:true,
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: true,
         },
         {
-          rel:"stylesheet",
-          href:"https://fonts.googleapis.com/css2?family=Gluten:wght@100..900&family=Grandstander:ital,wght@0,100..900;1,100..900&display=swap",
+          rel: 'preconnect',
+          href: 'https://cdn.jsdelivr.net/',
+          crossorigin: true,
         },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Gluten:wght@100..900&family=Grandstander:ital,wght@0,100..900;1,100..900&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/comic-mono@0.0.1/index.css',
+        }
       ]
     }
   }
@@ -23,21 +32,28 @@ export default {
 </script>
 
 <template>
-  <div class="default-layout">
+  <div class='default-layout'>
     <header>
-        <h1><nuxt-link to="/">Alex's Very Professional Portfolio</nuxt-link></h1>
+      <h1>
+        <nuxt-link to='/'>Alex's Very Professional Portfolio</nuxt-link>
+      </h1>
     </header>
     <main>
-        <Nuxt />
+      <Nuxt />
     </main>
   </div>
 </template>
 
 <style>
-html,body{
+html, body {
   font-family: 'Grandstander', sans-serif;
 }
-h1,h2,h3,h4,h5,h6{
-    font-family: 'Gluten', cursive;
-  }
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Gluten', cursive;
+}
+
+code, code[class*="language-"], pre[class*="language-"]{
+  font-family: 'Comic Mono', monospace;
+}
 </style>
